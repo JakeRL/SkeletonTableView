2d374dc1faf5ea7674a8ea84fb4b3a83757e9ea0
Jake Robinson-Lieberman <jake.rl@p-dv-mac-jrl001.kcura.corp>
2020-03-10 14:07:09 -0500

    skeleton working properly programmatically!

---
+++ SkeletonTableView/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SkeletonTableView
+//
+//  Created by Jake Robinson-Lieberman on 3/10/20.
+//  Copyright © 2020 Jake Robinson-Lieberman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    lazy var window: UIWindow? = UIWindow()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window?.rootViewController = UINavigationController(rootViewController: HumanListTableViewController())
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
